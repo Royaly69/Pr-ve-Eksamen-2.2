53b7da8ca3f4ae182efa58a8c07f18152c84d67e
Royaly69 <iownsteam@hotmail.com>
2024-11-28 10:08:15 +0100

    Add project files.

---
+++ Prøve Eksamen/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Prøve_Eksamen
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int answer=0;
+
+            Console.WriteLine("Indtast et tal fra og med 1 til og med 500");
+
+            try
+            {
+                answer = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Main();
+            }
+            if (answer >= 1 && answer <= 500)
+            {
+                Console.WriteLine(answer);
+            }
+            else if (answer ==-1)
+            {
+                Environment.Exit(answer);
+            }
+            else
+            {
+                Main();
+            }
+
+
+        }
+    }
+}
